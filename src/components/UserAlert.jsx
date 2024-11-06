455462a8ddac8bb70af941883dd2c612dc0d977f
Subham-Das-98 <das198subham@gamil.com>
2024-11-06 15:15:59 +0530

    optimize UserAlert.jsx component

--- src/components/UserAlert.jsx
+++ src/components/UserAlert.jsx
@@ -2,18 +2,20 @@ import React from "react";
 import AlertMessage from "./AlertMessage";
 import { useSelector } from "react-redux";
 
-
 function UserAlert() {
-  const alerts = useSelector(state => state.alertMessages);
+  const alerts = useSelector((state) => state.alertMessages);
 
   return (
     <>
       <div className="fixed bottom-4 left-4">
         {alerts.map((alertMsg) => (
           <li className="list-none" key={alertMsg.id}>
-          <AlertMessage alertMessage={alertMsg.message} messageId={alertMsg.id} />
+            <AlertMessage
+              alertMessage={alertMsg.message}
+              messageId={alertMsg.id}
+            />
           </li>
-        )) }
+        ))}
       </div>
     </>
   );
